620f07de4ff4a8ed508f448fa3f8b3c8a41dd7ab
Purvansh Singh <49719371+Purvanshsingh@users.noreply.github.com>
2020-07-09 09:13:47 +0530

    Impute values and Type conversion method completed

--- EDA.py
+++ EDA.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import logger
 
 
 class DataPreprocessor:
-    def __init__(self,logger_object):
+    def __init__(self, file_object, logger_object):
+        self.file_object = file_object
         self.logger_object = logger_object
 
     def impute_missing_values(self, data, mv_flag=None, target=None, strategy='median', impute_val=None,
@@ -22,7 +24,8 @@ class DataPreprocessor:
                 --‘mode’ : default for categorical variables
                 --‘fixed’ : replaces all missing values with a fixed ‘explicitly specified’ value
 
-                impute_val: None(default), can be assigned a value to be used for imputation in ‘fixed’ strategy
+                impute_val: None(default), can be assigned a value to be used for imputation i
+                n ‘fixed’ strategy
 
                 missing_vals: None(default), a list/tuple of missing value indicators. By default,
                  it considers only NaN as missing. Dictionary can be passed to consider different missing values
@@ -31,50 +34,63 @@ class DataPreprocessor:
                 mv_flag: None(default), can be passed list/tuple of columns as input for which it creates missing
                 value flags
 
+                On Exception: Write the exception in the log file. Raise an exception with the appropriate error message
+
                 return: A DataFrame with missing values imputed
 
                 Written By: Purvansh singh
                 Version: 1.0
                 Revisions: None
                 """
-        if isinstance(data, pd.DataFrame) and not data.empty:
-            dataframe = data
+        self.logger_object.log(self.file_object, "Entered into impute_missing_values method.")
+        try:
+            if isinstance(data, pd.DataFrame) and not data.empty:
+                self.logger_object.log(self.file_object, "Non-empty DataFrame object Identified")
+                dataframe = data
 
-            if mv_flag is True:
-                # Converting missing_vals to Nan Values
-                if missing_vals:
-                    dataframe.replace(missing_vals, np.nan, inplace=True)
-                #  Checking for Missing Values in Dependent Variable
-                if dataframe[target].isna().any():
-                    dataframe = dataframe[dataframe[
-                        target].notna()].copy()  # Selecting the Dataframe With No missing values in Dependent column
-                # Checking for Missing Values in Independent Variables
-                Missing_data_columns = dataframe.columns[
-                    dataframe.isna().any()].tolist()  # Finding Columns with the missing data from dataframe
-                if strategy == 'fixed':  # checking if strategy == fixed
-                    dataframe.fillna(impute_val,
-                                     inplace=True)  # Filling the Nan values with the imputed value from user
+                if mv_flag is True:
+                    self.logger_object.log(self.file_object, "my_flag found True Imputing Dataframe.")
+                    # Converting missing_vals to Nan Values
+                    if missing_vals:
+                        dataframe.replace(missing_vals, np.nan, inplace=True)
+                    #  Checking for Missing Values in Dependent Variable
+                    if dataframe[target].isna().any():
+                        dataframe = dataframe[dataframe[
+                            target].notna()].copy()  # Selecting the Dataframe With No missing values in Dependent column
+                    # Checking for Missing Values in Independent Variables
+                    Missing_data_columns = dataframe.columns[
+                        dataframe.isna().any()].tolist()  # Finding Columns with the missing data from dataframe
+                    if strategy == 'fixed':  # checking if strategy == fixed
+                        dataframe.fillna(impute_val,
+                                         inplace=True)  # Filling the Nan values with the imputed value from user
+                    else:
+                        for columns in Missing_data_columns:  # Iterating over the columns having Nan Values
+                            if dataframe[columns].dtype == 'object':  # Checking for the categorical data
+                                Mode = dataframe[columns].mode()[0]
+                                dataframe[columns].fillna(Mode,
+                                                          inplace=True)  # Imputing Nan values with mode of the column
+                            else:
+                                if strategy == 'median':  # checking if the strategy == median
+                                    Median = dataframe[columns].median()
+                                    dataframe[columns].fillna(Median,
+                                                              inplace=True)  # Imputing Nan values with median of the column
+                                else:  # The only strategy remains is mean
+                                    Mean = dataframe[columns].mean()
+                                    dataframe[columns].fillna(Mean,
+                                                              inplace=True)  # Imputing Nan values with mean of the column
+
                 else:
-                    for columns in Missing_data_columns:  # Iterating over the columns having Nan Values
-                        if dataframe[columns].dtype == 'object':  # Checking for the categorical data
-                            Mode = dataframe[columns].mode()[0]
-                            dataframe[columns].fillna(Mode, inplace=True)  # Imputing Nan values with mode of the column
-                        else:
-                            if strategy == 'median':  # checking if the strategy == median
-                                Median = dataframe[columns].median()
-                                dataframe[columns].fillna(Median,
-                                                          inplace=True)  # Imputing Nan values with median of the column
-                            else:  # The only strategy remains is mean
-                                Mean = dataframe[columns].mean()
-                                dataframe[columns].fillna(Mean,
-                                                          inplace=True)  # Imputing Nan values with mean of the column
-
+                    self.logger_object.log(self.file_object, "my_flag found False")
             else:
-                pass
+                raise Exception("No DataFrame Found")
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   "Error Occurred in impute_missing_values, Error statement: " + str(e))
+        else:
+            self.logger_object.log(self.file_object, "Imputed DataFrame Returned Successfully")
+            return dataframe
 
-        return dataframe
-
-    def type_conversion(self,dataset, cat_to_num=None,num_to_cat=None):
+    def type_conversion(self, dataset, cat_to_num=None, num_to_cat=None):
         '''
 
             Method Name: type_conversion
@@ -93,37 +109,42 @@ class DataPreprocessor:
 
             return: A DataFrame with column types changed as per requirement
 
+            On Exception : Write the exception in the log file. Raise an exception with the appropriate error message
+
             Written By: Purvansh singh
             Version: 1.0
             Revisions: None
         '''
         self.logger_object.log("Entered into type_conversion method.")
-        if isinstance(dataset, pd.DataFrame) and not dataset.empty:
-            if cat_to_num is not None:
-                for column in cat_to_num:
-                    try:
+        try:
+            if isinstance(dataset, pd.DataFrame) and not dataset.empty:
+                self.logger_object.log(self.file_object, "Non-empty DataFrame object Identified")
+                if cat_to_num is not None:
+                    for column in cat_to_num:
                         dataset[column] = pd.to_numeric(dataset[column])
-                    except Exception as e:
-                        print(e)
 
-            if num_to_cat is not None:
-                for column in num_to_cat:
-                    try:
-                        dataset[column] = dataset[column].astype('object')
-                    except Exception as e:
-                        print(e)
+                if num_to_cat is not None:
+                    for column in num_to_cat:
+                        try:
+                            dataset[column] = dataset[column].astype('object')
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   "Error Occurred in type_conversion method, Error statement: " + str(e))
+            #raise Exception(str(e))
 
+        else:
+            self.logger_object.log(self.file_object, "type_converted DataFrame Returned Successfully")
             return dataset
 
 
-
-
 if __name__ == '__main__':
+    log = logger.App_Logger()
     df = pd.read_csv('train.csv')
+    file = open('log.txt', "a")
     print(df.shape)
     print(df.isna().sum())
-    final = DataPreprocessor().impute_missing_values(df, True, 'is_promoted', 'fixed', 5000,
-                                                     {'recruitment_channel': 'other'})
+    final = DataPreprocessor(file, logger.App_Logger()).impute_missing_values('123', True, 'is_promoted', 'fixed', 5000,
+                                                                              {'recruitment_channel': 'other'})
 
     print(final.head())
     print(final.isna().sum())
